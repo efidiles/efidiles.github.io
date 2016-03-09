d44221c40537eab7f72ba3cf104da4586d849192
Eduard Fidiles <efidiles@gmail.com>
2016-03-09 23:50:35 +0000

    Add bash script to start

---
+++ start.sh
@@ -0,0 +1,11 @@
+apt-get install ruby-dev
+apt-get install make
+gem update --system
+
+gem install jekyll
+
+gem install jekyll-paginate
+gem install jekyll-sitemap
+
+gem update jekyll
+jekyll serve --watch
